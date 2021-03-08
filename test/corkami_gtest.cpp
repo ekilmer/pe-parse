e27966026dd6be18b977d8010f6ea499f3af2158
Eric Kilmer <eric.d.kilmer@gmail.com>
2021-03-08 18:16:14 -0500

    Add known failing corkami tests

--- test/corkami_gtest.cpp
+++ test/corkami_gtest.cpp
@@ -1,6 +1,9 @@
+#include <gtest/gtest.h>
 #include <pe-parse/parse.h>
 
-#include <gtest/gtest.h>
+#include <filesystem>
+#include <string>
+#include <unordered_set>
 
 #include "utils.h"
 
@@ -13,11 +16,39 @@ namespace peparse {
 
 class CorkamiTestSuite : public testing::TestWithParam<fs::path> {};
 
+static const std::unordered_set<std::string> kKnownPEFailure{
+    "virtsectblXP.exe", "maxsec_lowaligW7.exe",
+    "maxsecXP.exe",     "nullSOH-XP.exe",
+    "tinyXP.exe",       "tinydllXP.dll",
+    "virtrelocXP.exe",  "foldedhdrW7.exe",
+    "maxvals.exe",      "d_nonnull.dll",
+    "reloccrypt.exe",   "d_resource.dll",
+    "fakerelocs.exe",   "lfanew_relocW7.exe",
+    "bigSoRD.exe",      "tinyW7.exe",
+    "reloccryptW8.exe", "standard.exe",
+    "exe2pe.exe",       "tinygui.exe",
+    "dllfwloop.dll",    "tinydrivXP.sys",
+    "tiny.exe",         "tinydll.dll",
+    "foldedhdr.exe",    "dllmaxvals.dll",
+    "reloccryptXP.exe", "dosZMXP.exe",
+    "tinyW7_3264.exe",  "dllfw.dll",
+    "hdrcode.exe",      "ibrelocW7.exe",
+    "d_tiny.dll",       "sc.exe"};
+
 TEST_P(CorkamiTestSuite, Corkami) {
   auto path = GetParam();
+  auto pe_name = path.filename().string();
+
   auto *p = ParsePEFromFile(path.c_str());
-  EXPECT_TRUE(p) << GetPEErrString() << " at " << GetPEErrLoc();
-  DestructParsedPE(p);
+
+  if (kKnownPEFailure.count(pe_name)) {
+    // Known failure
+    EXPECT_EQ(p, nullptr)
+        << "Previously failing test now passes! Remove from set";
+  } else {
+    EXPECT_NE(p, nullptr) << GetPEErrString() << " at " << GetPEErrLoc();
+    DestructParsedPE(p);
+  }
 }
 
 INSTANTIATE_TEST_SUITE_P(CorkamiTests,
--- test/corkami_test.cpp
+++ test/corkami_test.cpp
@@ -1,6 +1,8 @@
-#include <pe-parse/parse.h>
-
 #include <catch2/catch.hpp>
+#include <string>
+#include <unordered_set>
+
+#include <pe-parse/parse.h>
 
 #include "utils.h"
 
@@ -10,18 +12,43 @@
 
 namespace peparse {
 
+static const std::unordered_set<std::string> kKnownPEFailure{
+    "virtsectblXP.exe", "maxsec_lowaligW7.exe",
+    "maxsecXP.exe",     "nullSOH-XP.exe",
+    "tinyXP.exe",       "tinydllXP.dll",
+    "virtrelocXP.exe",  "foldedhdrW7.exe",
+    "maxvals.exe",      "d_nonnull.dll",
+    "reloccrypt.exe",   "d_resource.dll",
+    "fakerelocs.exe",   "lfanew_relocW7.exe",
+    "bigSoRD.exe",      "tinyW7.exe",
+    "reloccryptW8.exe", "standard.exe",
+    "exe2pe.exe",       "tinygui.exe",
+    "dllfwloop.dll",    "tinydrivXP.sys",
+    "tiny.exe",         "tinydll.dll",
+    "foldedhdr.exe",    "dllmaxvals.dll",
+    "reloccryptXP.exe", "dosZMXP.exe",
+    "tinyW7_3264.exe",  "dllfw.dll",
+    "hdrcode.exe",      "ibrelocW7.exe",
+    "d_tiny.dll",       "sc.exe"};
+
 TEST_CASE("Corkami PEs smoketest", "[corkami]") {
   for (auto path : PEFilesInDir(CORKAMI_PE_PATH)) {
-    DYNAMIC_SECTION(path.filename()) {
+    auto pe_name = path.filename().string();
+    DYNAMIC_SECTION(pe_name) {
       auto *p = ParsePEFromFile(path.c_str());
-      if (p == nullptr) {
-        FAIL(GetPEErrString() + " at " + GetPEErrLoc());
+
+      if (kKnownPEFailure.count(pe_name)) {
+        CHECKED_ELSE(p == nullptr) {
+          FAIL("Previously failing test now passes! Remove from set");
+        }
+      } else {
+        CHECKED_ELSE(p != nullptr) {
+          FAIL(GetPEErrString() + " at " + GetPEErrLoc());
+        }
+        DestructParsedPE(p);
       }
-      DestructParsedPE(p);
-      SUCCEED();
     }
   }
-  WARN("Could not find Corkami dataset");
 }
 
 } // namespace peparse
